4e45640c8e1ece78cc010bfe6c86b278c98f0cc6
pedritto19 <pedrosusuke@gmail.com>
2024-06-05 11:06:27 -0300

    reset

--- main.py
+++ main.py
@@ -1,27 +1,11 @@
 import os
-import tiktoken
-from tokenizers import ByteLevelBPETokenizer
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
-tokenizer = ByteLevelBPETokenizer()
 
 
-with open('textos/texto', 'r', encoding='utf-8') as file:
-    dados = file.read()
-    
-PATH = 'textos/texto'
 
-# Initialize a ByteLevelBPETokenizer
-tokenizer = ByteLevelBPETokenizer()
-# Treinar o tokenizer
-tokenizer.train(files=[PATH], vocab_size=52_000, min_frequency=2, special_tokens=[
-    "<s>",
-    "<pad>",
-    "</s>",
-    "<unk>",
-    "<mask>",
-]) 
+
     
 @app.route('/')
 def home():
@@ -34,7 +18,7 @@ def home():
 def search():
     query = request.form['query']
 
-    print(tokenizer.encode(query).ids)
+
     return render_template('search.html', query=query)
 
 
